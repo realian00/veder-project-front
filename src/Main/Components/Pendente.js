b7dbd0f6eb6068a5e180801c732865677fb46a99
Your Namerealian00 <renato.gianelli@hotmail.com>
2022-03-04 16:29:01 -0300

    updated cards

--- src/Main/Components/Pendente.js
+++ src/Main/Components/Pendente.js
@@ -17,7 +17,7 @@ const Pendente = (props) => {
 
         <ul className="list p10 ph0 ma3">
           {filteredDatabase.map((item, i) => (
-            <li className="grow pointer" key={item._id}><Card id={item._id} key={item._id} cliente={item.cliente} os={item.os} nf={item.nf} entrada={item.entrada} orcamento={item.orcamento} aprovado={item.aprovado} status={item.status} produto={item.produto} obs={item.obs} onClick={props.click} /></li>
+            <li className="grow pointer" key={item._id}><Card id={item._id} key={item._id} cliente={item.cliente} os={item.os} nf={item.nf} entrada={item.entrada} orcamento={item.orcamento} aprovado={item.aprovado} status={item.status} produto={item.produto} obs={item.obs} pendencia={item.pendencia} onClick={props.click} /></li>
           ))}
         </ul>
       </div>
